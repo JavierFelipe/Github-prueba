d4eb25b09678fd9ec320748e5383a1cc21ee2ed8
JavierFelipe <jarodriguezac@unal.edu.co>
2025-03-21 00:18:23 -0500

    Ejemplos de ejercicios

--- Prueba.py
+++ Prueba.py
@@ -1,3 +1,53 @@
+#########################
 
-print("Ronaldo")
+#mensaje = "Real \"Madrid\""
 
+#print(mensaje)
+
+#########################
+
+#otro_mensaje = ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+#                    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+
+#print(Otro_mensaje)
+
+#########################
+
+#nombre = "Zinedine"
+
+#nombre_completo = nombre + "Zidane"
+
+#print(nombre_completo)
+
+#########################
+
+#for num in range(2, 10):
+
+    #if num % 2 == 0:
+        #print(f"El siguiente número es par: {num}")
+
+        #continue
+
+    #print(f"El siguiente número es impar: {num}")
+
+##########################
+
+#l = [1, 2, 3, 4, 5]
+
+#t = len(l)
+
+#def elementos_repetidos():
+    #r = 0
+
+    #for x in range(1, t):
+        #if l[r] != l[x]:
+            #r = x
+            #if r == 4:
+                #print("No hay elementos repetidos")
+            #continue
+        #print("Hay elementos repetidos")
+        #break
+
+#elementos_repetidos()
+
+###########################
